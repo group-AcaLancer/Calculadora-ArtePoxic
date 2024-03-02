d748c7bb1bce1e8a29e7b3e77503167826815662
Steven Contreras <132318055+Jefsteven22@users.noreply.github.com>
2024-03-02 17:51:21 -0500

    actualizacion responsivo Steven

--- src/components/IrregularFilling/IrregularFilling.jsx
+++ src/components/IrregularFilling/IrregularFilling.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./IrregularFilling.css";
 import CalculatorResult from "../../shared/results/CalculatorResult";
 import BtnBefore from "../../shared/btnBefore/BtnBefore";
@@ -10,16 +9,21 @@ import { useForm } from "react-hook-form";
 const IrregularFilling = () => {
   const [mostrarCompleto, setMostrarCompleto] = useState(true);
 
-  const {register, reset, handleSubmit, formState: {errors}} = useForm()
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const submit = (data) => { 
-    console.log(data)
+  const submit = (data) => {
+    console.log(data);
 
     reset({
-      area: '',
-      espesor: ''
-    })
-  }
+      area: "",
+      espesor: "",
+    });
+  };
 
   const toggleMostrarCompleto = () => {
     setMostrarCompleto(!mostrarCompleto);
@@ -36,38 +40,52 @@ const IrregularFilling = () => {
           />
           <h2 className="irregular-filling__header-title">Relleno Irregular</h2>
         </div>
-        <p className={`irregular-filling__paragraph ${mostrarCompleto ? "onparagraph" : ""}`}>
-              Calculo de Rendimiento <br />
-              En caso de una pieza irregular para calcular la cantidad de
-              producto, es necesario dividir la forma en figuras regulares mas
-              pequeñas e ir calculando sus medidas.
-              <br />
-              <br />
-              En caso de que eso no sea posible, dada la irregularidad,
-              recomendamos que proceda de la siguiente manera: <br />
-              <br />
-              Prepare una cantidad de resina que considere insuficiente:
-              <br />
-              Aplique la mezcla directamente en el molde, según el nivel
-              alcanzado puede calcular cuanto le puede hacer falta.
-              <br />
-              <br />
-              No hay una técnica precisa para calcular formas irregulares.
-              <br />
-              <br />
-              Ingrese los datos estimados de la figura.
-              <br />
-              <br />
+        <p
+          className={`irregular-filling__paragraph ${
+            mostrarCompleto ? "onparagraph" : ""
+          }`}
+        >
+          Calculo de Rendimiento <br />
+          En caso de una pieza irregular para calcular la cantidad de producto,
+          es necesario dividir la forma en figuras regulares mas pequeñas e ir
+          calculando sus medidas.
+          <br />
+          <br />
+          En caso de que eso no sea posible, dada la irregularidad, recomendamos
+          que proceda de la siguiente manera: <br />
+          <br />
+          Prepare una cantidad de resina que considere insuficiente.
+          <br />
+          Aplique la mezcla directamente en el molde, según el nivel alcanzado
+          puede calcular cuanto le puede hacer falta.
+          <br />
+          <br />
+          No hay una técnica precisa para calcular formas irregulares.
+          <br />
+          <br />
+          Ingrese los datos estimados de la figura.
+          <br />
+          <br />
         </p>
         <button
           className="irregularFilling__btn-more"
           onClick={toggleMostrarCompleto}
         >
-          {
-            mostrarCompleto 
-            ? <div className="container-chevron">Mostrar Más <span className="chevron"><i className='bx bx-chevron-down'></i></span></div> 
-            : <div className="container-chevron">Mostrar Menos <span className="chevron"><i className='bx bx-chevron-up'></i></span></div>
-          }
+          {mostrarCompleto ? (
+            <div className="container-chevron">
+              Mostrar Más{" "}
+              <span className="chevron">
+                <i className="bx bx-chevron-down"></i>
+              </span>
+            </div>
+          ) : (
+            <div className="container-chevron">
+              Mostrar Menos{" "}
+              <span className="chevron">
+                <i className="bx bx-chevron-up"></i>
+              </span>
+            </div>
+          )}
         </button>
       </div>
 
@@ -83,9 +101,13 @@ const IrregularFilling = () => {
               name="area"
               step="any"
               placeholder="Área (cm²)"
-              {...register('area', {required:true})}
+              {...register("area", { required: true })}
             />
-            {errors.area && <span className='irregular-filling__form-message-err'>* Area es requireda</span>}
+            {errors.area && (
+              <span className="irregular-filling__form-message-err">
+                * Area es requireda
+              </span>
+            )}
           </div>
 
           <div className="irregular-filling__form-content-input">
@@ -99,14 +121,16 @@ const IrregularFilling = () => {
               name="espesor"
               step="any"
               placeholder="Espesor (mm)"
-              {...register('espesor', {required:true})}
+              {...register("espesor", { required: true })}
             />
-            {errors.espesor && <span className='irregular-filling__form-message-err'>* Espesor es requiredo*</span>}
+            {errors.espesor && (
+              <span className="irregular-filling__form-message-err">
+                * Espesor es requiredo*
+              </span>
+            )}
           </div>
         </div>
-        <div
-          className="irregular-filling__form-button-content"
-        >
+        <div className="irregular-filling__form-button-content">
           <BtnBefore url={"/seleccion_de_forma"} />
           <button className="irregular-filling__form-button">Calcular</button>
         </div>
